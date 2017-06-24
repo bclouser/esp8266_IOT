9e2f94963b55d9f8715b3808117c55bb34ff878c
Ben Clouser <bclouse91@gmail.com>
2017-06-24 15:08:48 -0700

    Build now using separate sdk and toolchain

--- buildForMac.sh
+++ buildForMac.sh
@@ -2,15 +2,16 @@
 
 # On a mac, we need a case sensitive volumte for building. I just made one and mounted it
 MOUNTED_CASE_SENSITIVE="/Volumes/case-sensitive"
-PATH_TO_SDK="${MOUNTED_CASE_SENSITIVE}/esp-open-sdk/sdk"
+PATH_TO_SDK="${MOUNTED_CASE_SENSITIVE}/ESP8266_NONOS_SDK-2.1.0"
 TTY_PORT=/dev/tty.SLAB_USBtoUART
-ESPTOOL="${MOUNTED_CASE_SENSITIVE}/esp-open-sdk/esptool/esptool.py"
+ESPTOOL="${MOUNTED_CASE_SENSITIVE}/esp-open-sdk_builtSeparate/esptool/esptool.py"
 #LIBTOOL="${MOUNTED_CASE_SENSITIVE}/esp-open-sdk/crosstool-NG/.build/xtensa-lx106-elf/build/build-gmp-host-x86_64-build_apple-darwin15.5.0/libtool"
 LIBTOOL=libtool
 BUILD_FLAVOR="release"
 
 
-export PATH=/Volumes/case-sensitive/esp-open-sdk/xtensa-lx106-elf/bin:$PATH
+#export PATH=/Volumes/case-sensitive/esp-open-sdk/xtensa-lx106-elf/bin:$PATH
+export PATH=/Volumes/case-sensitive/esp-open-sdk_builtSeparate/xtensa-lx106-elf/bin:$PATH
 cd shadeControl
 
 # --debug=b
